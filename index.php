abdd73d55bfd337b74fb322a0e4ae8f6bbafe050
jeremy <jeremy@tsjee.ru>
2012-12-16 02:21:22 +0900

    created files

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+
+
+
+?>
+<!doctype html>
+<!--[if lt IE 7 ]> <html class="no-js ie6" lang="en"> <![endif]-->
+<!--[if IE 7 ]>    <html class="no-js ie7" lang="en"> <![endif]-->
+<!--[if IE 8 ]>    <html class="no-js ie8" lang="en"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+<html>
+    <head>
+        <link href="css/style.css" media="screen" rel="stylesheet" type="text/css">
+    </head>
+    <body>
+        <div id="main">
+        <div id="search-box">
+            <input type="text"  />
+            <input type="submit" value="search"/>
+        </div>
+        </div>
+    </body>
+    <script src="js/jquery.js" type="text/javascript"></script>
+    <script src="js/script.js" type="text/javascript"></script>
+</html>
